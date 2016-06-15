94e369e7688268a7cd3f25407ddc74ab986e90fe
takitaki21 <hrxdr280@yahoo.co.jp>
2016-06-15 17:10:39 +0900

    逆ポーランド記法（未完成）
    数値入力後動作停止

--- pr.c
+++ pr.c
@@ -1,66 +1,66 @@
 	#include <stdio.h>
-        int stack[100];
-		int p,i;
-		int a,b;
-		char c[100];
+int stack[100];
+int p,i;
+int a,b;
+char c[100];
 
-		 void push(int i){
-			stack[p++]=i;
-			i++;
-		}
+void push(int i){
+	stack[p++]=i;
+	i++;
+}
 
-		int pop() {
-		int i;
-			p--;
-			i=stack[p-1];
+int pop() {
+	int i;
+	p--;
+	i=stack[p-1];
 
-			return(i);
-		}
+	return(i);
+}
 
-		int clac(char *c){ 
-		  	while(*c){
-		  		 switch (*c) { 
-		             case '+':
-		                  a=pop();
-		                  b=pop();
-		                  push(b+a);
-		                  break;
+int clac(char *c){ 
+	while(*c){
+		switch (*c) { 
+			case '+':
+			a=pop();
+			b=pop();
+			push(b+a);
+			break;
 
-		              case '-':
-		                  a=pop();
-		                  b=pop();
-		                  push(b-a);
-		                  break;
+			case '-':
+			a=pop();
+			b=pop();
+			push(b-a);
+			break;
 
-		              case '*':
-		                  a=pop();
-		                  b=pop();
-		                  push(b*a);
-		                  break;
+			case '*':
+			a=pop();
+			b=pop();
+			push(b*a);
+			break;
 
-		              case '/':
-		                  a=pop();
-		                  b=pop();
-		                   if (a==0)
-		                      break;
+			case '/':
+			a=pop();
+			b=pop();
+			if (a==0)
+				break;
 
-		                  push(b/a);
-		                  break;
+			push(b/a);
+			break;
 
-		              case '=': printf("%d\n",pop() );
-		       return 0;
+			case '=': printf("%d\n",pop() );
+			return 0;
 
-		              default : push(atoi(c));
-		              break;
-		            }	 
-		      c++;      
-		    }
-		    return pop();
-		}
-		
-		
- int main(){
+			default : push(atoi(c));
+			break;
+		}	 
+		c++;      
+	}
+	return pop();
+}
+
+
+int main(){
 	char c[100];		
 	scanf("%s",*c);
 	printf("%d\n", clac(c) );
-	}
+}
