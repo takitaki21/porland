03fec96cd65c8beaa58e3ab79dab9c99ec07210d
takitaki21 <hrxdr280@yahoo.co.jp>
2016-06-23 15:28:31 +0900

    pr.c

--- pr.c
+++ pr.c
@@ -1,60 +1,65 @@
-#include <stdio.h>
-#include <string.h>
-int stack[100];
-int p=0;
+	#include <stdio.h>
+	#include <string.h>
+	int stack[100];
+	int p=0;
 
 
-void push(int h , int buff){
-	stack[h]=buff;
-}
-int pop() {
-    return stack[--p];
-}
+	void push(int h , int buff){
+		stack[h]=buff;
+	}
+	int pop() {
+	    return stack[--p];
+	}
 
-int main(){
-	int h = 0;	
-	int a=0,b=0 ;
-	char c[100];
+	int main(){
+		int h = 0;	
+		int a=0,b=0 ;
+		char c[100];
+	printf ("this is porland\n");
 	scanf("%s",c);
+
 	while(h < strlen(c)){
-		switch(c[h]){ 
+			switch(c[h]){ 
 
-			case '+':
-			a=pop();
-			b=pop();
-			push(0,b+a);
-			p=1;
-			break;
+				case '+':
+				a=pop();
+				b=pop();
+				push(0,b+a);
+				p=1;
+				break;
 
-			case '-':
-			a=pop();
-			b=pop();
-			push(0,b-a);
-			p=1;
-			break;
+				case '-':
+				a=pop();
+				b=pop();
+				push(0,b-a);
+				p=1;
+				break;
 
-			case '*':
-			a=pop();
-			b=pop();
-			push(0,b*a);
-			p=1;
-			break;
+				case '*':
+				a=pop();
+				b=pop();
+				push(0,b*a);
+				p=1;
+				break;
 
-			case '/':
-			a=pop();
-			b=pop();
-            push(0,b/a);
-            p=1;
-			break;
+				case '/':
+				a=pop();
+				b=pop();
+	            push(0,b/a);
+	            p=1;
+				break;
 
-			default :
-			push(p++,(int)c[h]-'0');
-		 break;
+				case '=':
+				printf("%d\n",stack[0]);
+	            return 0;
+				
+				default :
+				push(p++,(int)c[h]-'0');
+			 break;
+			}
+			h++;
+		   }
 		}
-		h++;
-	}
-	printf("%d\n",stack[0]);	      
-	}
 
 
 
